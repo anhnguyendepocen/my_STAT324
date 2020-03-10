b079ddbb6353165a3dc40f0eb5662cea227231b0
rmtrane <rmtrane@gmail.com>
2020-03-10 14:01:55 -0500

    Update index

--- docs/lecture04/student_sampling.R
+++
@@ -1,9 +0,0 @@
-student_names <- readr::read_csv(here::here("csv_data/student_names.csv"))
-
-continue <- 0
-
-while(continue != 'stop'){
-  cat(sample(student_names$name, size = 1))
-  continue <- readline()
-}
-
