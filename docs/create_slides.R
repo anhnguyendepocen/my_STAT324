7fc5949b254a54acd6fc171263ad1156dab7df30
rmtrane <rmtrane@gmail.com>
2020-03-13 14:29:39 -0500

    Update

---
+++ docs/create_slides.R
@@ -0,0 +1,19 @@
+file <- commandArgs(trailingOnly = TRUE)[[1]]
+
+print(file)
+
+my_moon_reader <- function(...) xaringan::moon_reader(..., css = c('../css/uwmadison.css', 'default-fonts', '../css/extra-classes.css'),
+                                                      lib_dir = 'libs',
+                                                      nature = list(
+                                                        titleSlideClass = c('center', 'top', '.title-slide'),
+                                                        highlightStyle = 'github',
+                                                        highlightLines = TRUE,
+                                                        countIncrementalSlides = FALSE,
+                                                        ratio = '16:10',
+                                                        navigation = list(scroll = FALSE)
+                                                      ),
+                                                      self_contained = TRUE)
+
+rmarkdown::render(file, "my_moon_reader")
+pagedown::chrome_print(stringr::str_replace(file, ".Rmd", ".html"), timeout = 1200)
+#rmarkdown::render(file)
